6a06151d87cf9919d70da48308a8ede1422111bc
99toxic <nsimpson1983@gmail.com>
2018-06-26 16:13:31 -0400

    Still working.

--- assets/php/login.php
+++ assets/php/login.php
@@ -3,12 +3,12 @@
 $username = $_POST[ 'name' ];
 $pass = $_POST[ 'password' ];
 
-if ( strlen( $username ) == 0 ) {
+if( $username != 'Nick' ) {
 	echo 'Forget something? Please enter your username.';
 } elseif ( $pass != 1234 ) {
 	echo 'Sorry wrong! Please enter the correct password.';
 }
 
 else {
-	echo file_get_contents("../../profile.html");
+	echo header("Location: ../../profile.html");
 }
